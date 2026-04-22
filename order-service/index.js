95ba926cce738d3eeba6df6c68b6979187eb2870
Muhand aldai Muhammed alsafi <88058837+MuhanadEldai@users.noreply.github.com>
2026-04-23 00:24:27 +0200

    Update index.js

--- order-service/index.js
+++ order-service/index.js
@@ -366,7 +366,7 @@ app.get('/', async (req, res) => {
 });
 
 // Get all orders with items
-app.get('/Orders', async (req, res) => {
+app.get('/ORders', async (req, res) => {
   if (isShuttingDown) {
     return res.status(503).json({ 
       success: false, 
